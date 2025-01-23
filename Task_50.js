12393de549250d46120d424faa836fbf737852ca
Ragavan R V <167912221+Ragav1102@users.noreply.github.com>
2025-01-23 12:10:01 +0530

    initial commit

---
+++ Task_50.js
@@ -0,0 +1,7 @@
+function info(name = "Ragav",age=21){
+    console.log(`Hello, ${name}! You are ${age} years old.`);
+}
+info();                     
+info("Alice");             
+info("Bob", 30);           
+info(undefined, 40);     
